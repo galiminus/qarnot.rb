90bdce9f31c022afe6773fcdd8f7739cdd897413
Victor Goya <victor@amusenetwork.com>
2017-07-21 12:57:22 +0200

    add profiles api

--- lib/qarnot/collections/profiles.rb
+++ lib/qarnot/collections/profiles.rb
@@ -3,6 +3,16 @@ require "qarnot/collections/base"
 module Qarnot
   module Collection
     class Profiles < Base
+      def all
+        JSON.parse(auth.root["profiles"].get.body).map do |name|
+          find(name)
+        end
+      end
+
+      def find(name)
+        data = JSON.parse(auth.root["profiles"][name].get.body)
+        Qarnot::Model::Profile.new({ auth: auth }.merge(data))
+      end
     end
   end
 end
--- lib/qarnot/models/profile.rb
+++ lib/qarnot/models/profile.rb
@@ -1,6 +1,18 @@
+require "qarnot/models/base"
+
 module Qarnot
   module Model
-    class Profile
+    class Profile < Base
+      attr_accessor :name, :constants
+
+      def initialize(params)
+        super params
+
+        update_attributes({
+          name:             params.delete(:name),
+          constants:        params.delete(:constants)
+        })
+      end
     end
   end
 end
